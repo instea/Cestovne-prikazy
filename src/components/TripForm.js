835d730375548904826a1a84ffdf8eb3f20cf910
Juraj Matuš <matus.juraj@yandex.com>
2017-07-04 17:03:13 +0200

    Validation added

--- src/components/TripForm.js
+++ src/components/TripForm.js
@@ -2,7 +2,7 @@ import './TripForm.css';
 import Trip from '../models/Trip';
 
 import React, { Component } from 'react';
-import {Row, Col, ButtonToolbar, Button, PageHeader, FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
+import {Row, Col, ButtonToolbar, Button, PageHeader, FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
 import Datetime from 'react-datetime';
 import 'react-datetime/css/react-datetime.css';
 import {connect} from 'react-redux';
@@ -10,21 +10,30 @@ import * as actions from '../dispatch/actions';
 import {Field, reduxForm} from 'redux-form';
 
 const ReduxDatetime = (field) => (
-  <Datetime {...field.input} dateFormat="D. M. YYYY" timeFormat="H:mm" />
+  <Row>
+    <FormGroup controlId={"fc" + field.id} validationState={field.meta.touched && field.meta.error ? "error" : undefined}>
+      <Col componentClass={ControlLabel} sm={2} smOffset={2}>
+        {field.label}
+      </Col>
+      <Col sm={6}>
+        <Datetime {...field.input} dateFormat="D. M. YYYY" timeFormat="H:mm" />
+        {field.meta.touched && field.meta.error && <HelpBlock>{field.meta.error}</HelpBlock>}
+      </Col>
+    </FormGroup>
+  </Row>
 );
 
 const ReduxInput = (field) => (
-  <FormControl type="text" {...field.input} />
-);
-
-const FormEl = props => (
   <Row>
-    <Col componentClass={ControlLabel} sm={2} smOffset={2}>
-      <ControlLabel>{props.label}</ControlLabel>
-    </Col>
-    <Col sm={6}>
-      {props.children}
-    </Col>
+    <FormGroup controlId={"fc" + field.id} validationState={field.meta.touched && field.meta.error ? "error" : undefined}>
+      <Col componentClass={ControlLabel} sm={2} smOffset={2}>
+        {field.label}
+      </Col>
+      <Col sm={6}>
+        <FormControl type="text" {...field.input} />
+        {field.meta.touched && field.meta.error && <HelpBlock>{field.meta.error}</HelpBlock>}
+      </Col>
+    </FormGroup>
   </Row>
 );
 
@@ -36,21 +45,9 @@ class TripForm extends Component {
         <Col sm={12}>
           <PageHeader>{this.props.isNew ? 'Add trip' : 'Edit trip'}</PageHeader>
           <form onSubmit={this.props.handleSubmit}>
-            <FormGroup controlId="trip-form">
-
-              <FormEl label="Starting at:">
-                <Field name="from" component={ReduxDatetime} />
-              </FormEl>
-
-              <FormEl label="Ending at:">
-                <Field name="to" component={ReduxDatetime} />
-              </FormEl>
-
-              <FormEl label="Place:">
-                <Field name="place" component={ReduxInput} />
-              </FormEl>
-
-            </FormGroup>
+            <Field name="from" label="Starting at:" id="from" component={ReduxDatetime} />
+            <Field name="to" label="Ending at:" id="to" component={ReduxDatetime} />
+            <Field name="place" label="Place:" id="place" component={ReduxInput} />
             <Row>
               <Col smOffset={4} sm={4}>
                 <ButtonToolbar>
@@ -66,6 +63,32 @@ class TripForm extends Component {
   }
 
 }
+
+const validate = (values) => {
+  const errors = {};
+
+  const from = values.from.toDate();
+  const to = values.to.toDate();
+  const now = new Date();
+
+  if (from <= now) {
+    errors.from = 'Must be in the future';
+  }
+
+  if (to <= now) {
+    errors.to = 'Must be in the future';
+  } else if (from > to) {
+    errors.to = 'Must be after the start';
+  }
+
+  if (!values.place) {
+    errors.place = 'Required'
+  } else if (values.place.length < 3) {
+    errors.place = 'Must be at least 3 characters long';
+  }
+
+  return errors;
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -88,5 +111,6 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(reduxForm({
-  form: 'trip'
+  form: 'trip',
+  validate
 })(TripForm));
