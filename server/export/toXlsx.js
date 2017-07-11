7e69647ffc635d99a777e0acfe624c2264b00330
Juraj Matuš <matus.juraj@yandex.com>
2017-07-11 17:13:59 +0200

    Excel library change

--- server/export/toXlsx.js
+++ server/export/toXlsx.js
@@ -1,25 +1,16 @@
-const XLSX = require('xlsx');
+const Excel = require('exceljs');
 const path = require('path');
 
 module.exports = async () => {
 
-  const workbook = XLSX.readFile(path.join(__dirname, '../templates/cestovny-prikaz.xlsx'));
-  const sheetName = workbook.SheetNames[0];
-  const sheet = workbook.Sheets[sheetName];
+  const workbook = new Excel.Workbook();
+  await workbook.xlsx.readFile(path.join(__dirname, '../templates/cestovny-prikaz.xlsx'));
+
+  // TODO - replacement logic
+  // TODO - save: why is data lost?
 
   const filename = path.join(__dirname, '../download', `cestovny-prikaz-${Math.round(Math.random() * 90000000 + 10000000)}.xlsx`);
-
-  const writePromise = new Promise((resolve, reject) => {
-    XLSX.writeFileAsync(filename, workbook, () => {
-      resolve();
-    });
-  });
-  try {
-    await writePromise;
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
+  await workbook.xlsx.writeFile(filename);
 
   return filename;
 
