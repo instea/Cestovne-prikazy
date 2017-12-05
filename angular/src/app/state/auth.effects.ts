eb7657622f6bed4413d02183fb918a52a3342e4b
Juraj Matuš <matus.juraj@yandex.com>
2017-12-05 14:50:14 +0100

    Simplified implementation of interval management

--- angular/src/app/state/auth.effects.ts
+++ angular/src/app/state/auth.effects.ts
@@ -20,18 +20,18 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/takeWhile';
 import { of } from 'rxjs/observable/of';
 import { interval } from 'rxjs/observable/interval';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Subscription } from 'rxjs/Subscription';
-import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class AuthEffects {
 
-  private refresherSubject?: Subject<{}>;
+  private refreshedJwt?: string;
 
   constructor(
     private actions: Actions,
@@ -51,34 +51,29 @@ export class AuthEffects {
       .map(jwt => new LoginSuccessfulAction({ jwt }))
       .catch(err => of(new LoginFailedAction({ message: err }))));
 
-  discardRefresher() {
-    if (this.refresherSubject) {
-      console.log('Closing subject for jwt refreshing');
-      this.refresherSubject.complete();
-      this.refresherSubject = undefined;
-    }
+  stopRefreshing() {
+    this.refreshedJwt = undefined;
   }
 
-  createRefresher() {
-    this.discardRefresher();
-    this.refresherSubject = new Subject();
-    interval(REFRESH_JWT_INTERVAL).subscribe(this.refresherSubject);
-    return this.refresherSubject;
+  createRefresher(jwt: string) {
+    this.stopRefreshing();
+    this.refreshedJwt = jwt;
+    return interval(REFRESH_JWT_INTERVAL).takeWhile(() => jwt === this.refreshedJwt);
   }
 
   @Effect() refreshJwt = this.actions
     .ofType(LOGIN_SUCCESSFUL)
-    .switchMap((action) => 
-      this.createRefresher().switchMap(() =>
+    .switchMap(({ payload: { jwt } }) => 
+      this.createRefresher(jwt).switchMap(() =>
         this.httpClient.post(REFRESH_JWT_URL, undefined, {
           headers: new HttpHeaders({
-            'Authorization': `Bearer ${action.payload.jwt}`
+            'Authorization': `Bearer ${jwt}`
           })
-        }).map((body: any) => new RefreshJwtAction({ jwt: body }))));
+        }).map((newJwt: any) => new RefreshJwtAction({ jwt: newJwt }))));
 
   @Effect({ dispatch: false }) logout = this.actions
     .ofType(LOGOUT)
-    .do(() => this.discardRefresher())
+    .do(() => this.stopRefreshing())
     .do(() => this.authService.logoutUser());
 
   @Effect() loadUserInfo = this.actions
