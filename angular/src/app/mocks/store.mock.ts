c9ded2e8687d2d9fa5610986404b9a18b7f05604
Stanislav Miklik <stanislav.miklik@gmail.com>
2017-12-11 10:15:45 +0100

    enhance nav-header tests

--- angular/src/app/mocks/store.mock.ts
+++ angular/src/app/mocks/store.mock.ts
@@ -1,11 +1,29 @@
 import { INITIAL_STATE } from './../state/root';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store, Action } from '@ngrx/store';
+import { set, merge } from 'lodash'
 
 export function mockStoreModule() {
-    const routerDefinitions = Object.keys(INITIAL_STATE).reduce((def, key) => Object.assign(def, { key: mockReducer }), {})
+    const routerDefinitions = Object.keys(INITIAL_STATE).reduce((def, key) => Object.assign(def, { [key]: mockReducer(key) }), {})
     return StoreModule.provideStore(routerDefinitions, INITIAL_STATE)
 }
 
-function mockReducer(state) {
-    return state
+const UPDATE_STATE = "UPDATE_STATE"
+
+function mockReducer(key: string) {
+    return function mockedReducer(state, action: Action) {
+        if (action.type === UPDATE_STATE) {
+            let { path, value } = action.payload
+            if (path.startsWith(key + '.')) {
+                path = path.substr(key.length + 1);
+                console.log('setting', path, value);
+                const change = set({}, path, value)
+                return merge({}, state, change)
+            }
+        }
+        return state
+    }
+}
+
+export function mockState(store: Store<any>, path: string, value: any) {
+    store.dispatch({ type: UPDATE_STATE, payload: { path, value } })
 }
--- angular/src/app/nav-header/nav-header.component.spec.ts
+++ angular/src/app/nav-header/nav-header.component.spec.ts
@@ -1,11 +1,18 @@
+import { UserInfo } from './../state/auth';
+import { AppState } from './../state/root';
+import { Store } from '@ngrx/store';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { mockStoreModule } from '../mocks/store.mock';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { mockStoreModule, mockState } from '../mocks/store.mock';
 
 import { NavHeaderComponent } from './nav-header.component';
 
 describe('NavHeaderComponent', () => {
   let component: NavHeaderComponent;
   let fixture: ComponentFixture<NavHeaderComponent>;
+  let de: DebugElement;
+  let store: Store<AppState>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,6 +26,7 @@ describe('NavHeaderComponent', () => {
   }));
 
   beforeEach(() => {
+    store = TestBed.get(Store);
     fixture = TestBed.createComponent(NavHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -27,4 +35,18 @@ describe('NavHeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not show right dropdown if not logged in', () => {
+    de = fixture.debugElement.query(By.css('#navbarDropdownMenuLink'));
+    expect(de).toBeFalsy();
+  });
+
+  it('should show right dropdown if logged in', () => {
+    mockState(store, 'auth.userInfo', { username: 'testName' })
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('#navbarDropdownMenuLink'));
+    expect(de).toBeTruthy();
+    expect(de.nativeElement.textContent).toContain('testName');
+  });
+
 });
