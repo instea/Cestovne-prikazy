b7be3b4c5efe023c837540f379457d3f77e34fc5
Martin Bielik <mx.bielik@gmail.com>
2017-12-13 15:23:17 +0100

    ci review issues

--- bin/test.sh
+++ bin/test.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
+set -e
+
 yarn
 CI=true yarn test
-RES=$?
-if [ $RES -ne 0 ]
-then
-  exit $RES
-fi
 yarn lint
-RES=$?
-if [ $RES -ne 0 ]
-then
-  exit $RES
-fi
 cd angular
 yarn
 yarn test --config karma.ci.js
-RES=$?
-if [ $RES -ne 0 ]
-then
-  exit $RES
-fi
 yarn lint
-RES=$?
-if [ $RES -ne 0 ]
-then
-  exit $RES
-fi
