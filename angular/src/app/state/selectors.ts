a6104be1c67ff18c33d78513263777f9ee6d39fb
Juraj Matuš <matus.juraj@yandex.com>
2017-12-15 14:07:23 +0100

    Leaves - list / calendar switcher

--- angular/src/app/state/selectors.ts
+++ angular/src/app/state/selectors.ts
@@ -5,8 +5,17 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'zen-observable';
 
 // AUTH SELECTORS
-export const isLoggedIn = (store: Store<AppState>) => store.select(state => !!state.auth.jwt);
-export const getJwt = (store: Store<AppState>) => store.select(state => state.auth.jwt);
-export const getUserInfo = (store: Store<AppState>) => store.select(state => state.auth.userInfo);
-export const isLoginInProgess = (store: Store<AppState>) => store.select(state => state.auth.loginInProgress);
-export const getLoginErrorMessage = (store: Store<AppState>) => store.select(state => state.auth.loginError);
+export const isLoggedIn = (store: Store<AppState>) =>
+  store.select(state => !!state.auth.jwt);
+export const getJwt = (store: Store<AppState>) =>
+  store.select(state => state.auth.jwt);
+export const getUserInfo = (store: Store<AppState>) =>
+  store.select(state => state.auth.userInfo);
+export const isLoginInProgess = (store: Store<AppState>) =>
+  store.select(state => state.auth.loginInProgress);
+export const getLoginErrorMessage = (store: Store<AppState>) =>
+  store.select(state => state.auth.loginError);
+
+// LEAVES SELECTORS
+export const getLeaveView = (store: Store<AppState>) =>
+  store.select(state => state.leaves.view);
