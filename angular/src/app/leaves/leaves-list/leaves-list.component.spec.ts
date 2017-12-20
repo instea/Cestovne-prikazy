4e9df35f8cacaf420026d13836bd10c022e62acb
Martin Bielik <mx.bielik@gmail.com>
2017-12-20 17:52:21 +0100

    Long leaves filter (#15)
    * filter to consider multi-month/year leaves
    
    * leave list component tests
    
    * failing test
    
    * fixed multi-year month filter

--- angular/src/app/leaves/leaves-list/leaves-list.component.spec.ts
+++ angular/src/app/leaves/leaves-list/leaves-list.component.spec.ts
@@ -1,4 +1,8 @@
 import { of } from 'rxjs/observable/of';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { AppState } from '../../state/root';
+import { Store } from '@ngrx/store';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
 import { LeavesService } from './../../services/leaves.service';
@@ -12,15 +16,59 @@ import { CalendarComponent } from 'ng-fullcalendar';
 import { LeavesCalendarComponent } from '../leaves-calendar/leaves-calendar.component';
 import { StoreModule } from '@ngrx/store';
 import { mockState, mockStoreModule } from '../../mocks/store.mock';
+import { LeaveType, LeaveState } from '../leave';
+import { User } from '../../login-page/user';
+
+const makeUser = id => {
+  const user = new User();
+  user.id = id;
+  user.firstName = id;
+  user.surname = id;
+  user.username = id;
+  return user;
+};
+
+const user1 = makeUser('1');
+const user2 = makeUser('2');
+const user3 = makeUser('3');
+
+const leaves = [
+  {
+    id: '1',
+    startDate: new Date(2017, 11, 5),
+    endDate: new Date(2017, 11, 5),
+    type: LeaveType.ANNUAL,
+    state: LeaveState.PENDING,
+    requester: user1,
+  },
+  {
+    id: '2',
+    startDate: new Date(2018, 11, 5),
+    endDate: new Date(2018, 11, 5),
+    type: LeaveType.ANNUAL,
+    state: LeaveState.PENDING,
+    requester: user2,
+  },
+  {
+    id: '3',
+    startDate: new Date(2017, 10, 5),
+    endDate: new Date(2019, 3, 1),
+    type: LeaveType.ANNUAL,
+    state: LeaveState.PENDING,
+    requester: user3,
+  },
+];
 
 describe('LeavesListComponent', () => {
   let component: LeavesListComponent;
   let fixture: ComponentFixture<LeavesListComponent>;
+  let store: Store<AppState>;
+  let de: DebugElement;
 
   beforeEach(
     async(() => {
       const lMockService = {
-        getLeaves: () => of([]),
+        getLeaves: () => of(leaves),
       };
       const hMockService = {
         numWorkDays: () => 0,
@@ -47,6 +95,7 @@ describe('LeavesListComponent', () => {
   );
 
   beforeEach(() => {
+    store = TestBed.get(Store);
     fixture = TestBed.createComponent(LeavesListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -54,5 +103,24 @@ describe('LeavesListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display all leaves', () => {
+    de = fixture.debugElement.query(By.css('#leaveList tbody'));
+    const rows = de.children;
+    expect(rows.length).toBe(3);
+    expect(rows[0].nativeElement.textContent).toContain(user1.getFullName());
+    expect(rows[1].nativeElement.textContent).toContain(user2.getFullName());
+    expect(rows[2].nativeElement.textContent).toContain(user3.getFullName());
+  });
+
+  it('should display filtered leaves', () => {
+    mockState(store, 'leaves.leaveListFilter', { months: [11], years: [2017] });
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css('#leaveList tbody'));
+    const rows = de.children;
+    expect(rows.length).toBe(2);
+    expect(rows[0].nativeElement.textContent).toContain(user1.getFullName());
+    expect(rows[1].nativeElement.textContent).toContain(user3.getFullName());
   });
 });
