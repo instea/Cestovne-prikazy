c942127a800491e71efed7752a13bd23f5fa0476
Stanislav Miklik <stanislav.miklik@gmail.com>
2017-12-21 14:37:20 +0100

    fix tests prerequisites

--- angular/src/app/leaves/leaves-export/leaves-export.component.spec.ts
+++ angular/src/app/leaves/leaves-export/leaves-export.component.spec.ts
@@ -1,6 +1,13 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/primeng';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs/observable/of';
 
 import { LeavesExportComponent } from './leaves-export.component';
+import { UsersService } from './../../services/users.service';
+import { mockStoreModule } from '../../mocks/store.mock';
 
 describe('LeavesExportComponent', () => {
   let component: LeavesExportComponent;
@@ -8,8 +15,19 @@ describe('LeavesExportComponent', () => {
 
   beforeEach(
     async(() => {
+      const mockService = {
+        getUsers: () => of([]),
+      };
       TestBed.configureTestingModule({
+        imports: [
+          ReactiveFormsModule,
+          CalendarModule,
+          BrowserAnimationsModule,
+          mockStoreModule(),
+        ],
+        schemas: [NO_ERRORS_SCHEMA],
         declarations: [LeavesExportComponent],
+        providers: [{ provide: UsersService, useValue: mockService }],
       }).compileComponents();
     })
   );
--- angular/src/app/state/leave.effects.spec.ts
+++ angular/src/app/state/leave.effects.spec.ts
@@ -2,12 +2,13 @@ import { LeavesService } from './../services/leaves.service';
 import { TestBed, inject } from '@angular/core/testing';
 import { EffectsTestingModule, EffectsRunner } from '@ngrx/effects/testing';
 import { LeavesEffects } from './leave.effects';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LeavesEffects', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [EffectsTestingModule],
-      providers: [LeavesEffects, { provide: LeavesService, useValue: {} }]
+      imports: [EffectsTestingModule, HttpClientTestingModule],
+      providers: [LeavesEffects, { provide: LeavesService, useValue: {} }],
     });
   });
 
