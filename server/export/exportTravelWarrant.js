b7ff7c17a3ef3860dab3e423bb6e4721f1fbc297
Juraj Matuš <matus.juraj@yandex.com>
2018-09-03 14:34:35 +0200

    Fixing exported file paging

--- server/export/exportTravelWarrant.js
+++ server/export/exportTravelWarrant.js
@@ -3,7 +3,7 @@ const path = require('path');
 const moment = require('moment');
 const dbSchema = require('../db/schema');
 const _ = require('lodash');
-const {compute: computeDiet} = require('../../src/data/Diets');
+const { compute: computeDiet } = require('../../src/data/Diets');
 
 const setCellValue = (cell, value) => {
   if (/^-?([0-9]+)(\.[0-9]+)?$/.test(value)) {
@@ -17,45 +17,56 @@ const setCellValue = (cell, value) => {
   }
 };
 
-module.exports = async ({userId, month}) => {
-
-  const user = await dbSchema.User.findOne({id: userId});
+module.exports = async ({ userId, month }) => {
+  const user = await dbSchema.User.findOne({ id: userId });
   const m_month = moment(month, 'YYYY-MM');
   const dateRange = [m_month, m_month.clone().add(1, 'month')];
 
   const trips = await dbSchema.Trip.find({
     userId: userId,
     departureTime: {
-      '$gte': dateRange[0].toDate(),
-      '$lt': dateRange[1].toDate()
+      $gte: dateRange[0].toDate(),
+      $lt: dateRange[1].toDate()
     }
   });
 
   const placeIds = trips.map(trip => trip.placeId);
-  const places = await dbSchema.Place.find({id: {
-    '$in': placeIds
-  }});
+  const places = await dbSchema.Place.find({
+    id: {
+      $in: placeIds
+    }
+  });
   const placesById = {};
-  places.forEach(place => placesById[place.id] = place);
-  await Promise.all(trips.map(async (trip) => {
-    trip.place = placesById[trip.placeId];
+  places.forEach(place => (placesById[place.id] = place));
+  await Promise.all(
+    trips.map(async trip => {
+      trip.place = placesById[trip.placeId];
 
-    const dur = moment.duration(Math.abs(moment(trip.arrivalTime).diff(trip.departureTime)));
-    const hours = dur.asHours();
-    const country = trip.place.country;
-    trip.dietTariff = await computeDiet(country, hours);
-    return true;
-  }));
+      const dur = moment.duration(
+        Math.abs(moment(trip.arrivalTime).diff(trip.departureTime))
+      );
+      const hours = dur.asHours();
+      const country = trip.place.country;
+      trip.dietTariff = await computeDiet(country, hours);
+      return true;
+    })
+  );
 
   const workbook = new Excel.Workbook();
-  await workbook.xlsx.readFile(path.join(__dirname, '../templates/cestovny-prikaz.xlsx'));
+  await workbook.xlsx.readFile(
+    path.join(__dirname, '../templates/cestovny-prikaz.xlsx')
+  );
   const sheet = workbook.getWorksheet(1);
 
   // Header
-  _.range(1, 5, true).map(rowIndex => sheet.getRow(rowIndex).eachCell((cell) =>
-    cell.value = _.template(cell.value)({
-      user: user
-    })));
+  _.range(1, 5, true).map(rowIndex =>
+    sheet.getRow(rowIndex).eachCell(
+      cell =>
+        (cell.value = _.template(cell.value)({
+          user: user
+        }))
+    )
+  );
 
   // Left part
   const lcols = ['A', 'C', 'F', 'H'];
@@ -73,13 +84,14 @@ module.exports = async ({userId, month}) => {
 
   let rowIndex = 8;
   trips.forEach(trip => {
-
     const row = sheet.getRow(rowIndex);
     const cells = lcols.map(col => row.getCell(col));
-    const values = ltemplates.map(tpl => tpl({
-      user: user,
-      trip: trip
-    }));
+    const values = ltemplates.map(tpl =>
+      tpl({
+        user: user,
+        trip: trip
+      })
+    );
 
     _.zip(cells, values).forEach(([cell, value]) => {
       setCellValue(cell, value);
@@ -90,10 +102,25 @@ module.exports = async ({userId, month}) => {
 
   // Right part
   const rows = [7, 9, 7, 8, 9, 10, 7, 8, 9, 10, 7, 7, 7];
-  const rcols = ['J', 'J', 'L', 'L', 'L', 'L', 'M', 'M', 'M', 'M', 'N', 'R', 'S'];
-  const getCells = (index) => _.zip(rows.map(row => row + 4 * index), rcols)
-    .map(([row, col]) => col + row)
-    .map(cellNum => sheet.getCell(cellNum));
+  const rcols = [
+    'J',
+    'J',
+    'L',
+    'L',
+    'L',
+    'L',
+    'M',
+    'M',
+    'M',
+    'M',
+    'N',
+    'R',
+    'S'
+  ];
+  const getCells = index =>
+    _.zip(rows.map(row => row + 4 * index), rcols)
+      .map(([row, col]) => col + row)
+      .map(cellNum => sheet.getCell(cellNum));
 
   const rtemplates = getCells(0).map(cell => {
     const tpl = _.template(cell.value, {
@@ -107,12 +134,13 @@ module.exports = async ({userId, month}) => {
 
   let index = 0;
   trips.forEach(trip => {
-
     const cells = getCells(index);
-    const values = rtemplates.map(tpl => tpl({
-      user: user,
-      trip: trip
-    }));
+    const values = rtemplates.map(tpl =>
+      tpl({
+        user: user,
+        trip: trip
+      })
+    );
 
     _.zip(cells, values).forEach(([cell, value]) => {
       setCellValue(cell, value);
@@ -122,15 +150,25 @@ module.exports = async ({userId, month}) => {
   });
 
   // Clearing cached formula results (that prevents Excel from performing calculations)
-  sheet.eachRow((row) => row.eachCell((cell) => {
-    if (cell.type === Excel.ValueType.Formula) {
-      cell.value = _.omit(cell.value, 'result');
-    }
-  }));
+  sheet.eachRow(row =>
+    row.eachCell(cell => {
+      if (cell.type === Excel.ValueType.Formula) {
+        cell.value = _.omit(cell.value, 'result');
+      }
+    })
+  );
 
-  const filename = path.join(__dirname, '../download', `cestovny-prikaz-${Math.round(Math.random() * 90000000 + 10000000)}.xlsx`);
+  // Page-break fix - enlarging the column, so that auto page-break was as expected
+  // The best way would be to set "colBreak", but this library loses it on parsing
+  // and doesn't support adding it programatically
+  sheet.getColumn(5).width = 15.56;
+
+  const filename = path.join(
+    __dirname,
+    '../download',
+    `cestovny-prikaz-${Math.round(Math.random() * 90000000 + 10000000)}.xlsx`
+  );
   await workbook.xlsx.writeFile(filename);
 
   return filename;
-
 };
