1f897704462b6bfcf6267882fafca4610a5403b3
Jozef Kamensky <jozef.kamensky@instea.co>
2019-07-25 12:26:37 +0200

    fix login page test, mock google auth service

--- angular/src/app/login-page/login-page.component.spec.ts
+++ angular/src/app/login-page/login-page.component.spec.ts
@@ -1,20 +1,24 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { mockStoreModule } from '../mocks/store.mock';
+import { AuthServiceMock } from '../mocks/google.auth.mock';
 
 import { LoginPageComponent } from './login-page.component';
+import { AuthService } from 'angularx-social-login';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginPageComponent],
-      imports: [ReactiveFormsModule, mockStoreModule()],
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [LoginPageComponent],
+        imports: [ReactiveFormsModule, mockStoreModule()],
+        providers: [{ provide: AuthService, useClass: AuthServiceMock }],
+      }).compileComponents();
     })
-      .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginPageComponent);
