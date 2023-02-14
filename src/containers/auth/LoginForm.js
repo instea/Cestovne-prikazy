76d81f4e2bd25890b580b2c79bef9f69f0db72d8
misosviso <michal.komora@protonmail.com>
2023-02-14 20:14:07 +0100

    replaced deprecated react-google-login module for @react-oauth/google

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -7,7 +7,7 @@ import {connect} from 'react-redux';
 import * as actions from '../../actions/authActions';
 import {compose} from 'react-apollo';
 import {bindActionCreators} from 'redux';
-import {GoogleLogin} from 'react-google-login';
+import { GoogleLogin } from '@react-oauth/google';
 import {getLoginResult} from '../../selectors/user';
 import LoginResultMessage from '../../components/LoginResultMessage';
 
@@ -21,14 +21,7 @@ class LoginForm extends Component {
           <Row>
             <Col xsOffset={4} xs={4} smOffset={3} sm={4} mdOffset={4} md={4} lgOffset={4} lg={4}>
               <ButtonToolbar>
-                <GoogleLogin
-                  clientId={process.env.REACT_APP_CLIENT_ID}
-                  buttonText="Login with Google"
-                  onSuccess={this.props.onGoogleSuccess}
-                  onFailure={onGoogleFailure}
-                  cookiePolicy={'single_host_origin'}
-                  hostedDomain={process.env.REACT_APP_HOSTED_DOMAIN || ''}
-                />
+                <GoogleLogin onSuccess={this.props.onGoogleSuccess} onError={onGoogleFailure} hosted_domain={process.env.REACT_APP_HOSTED_DOMAIN || ''}/>
               </ButtonToolbar>
             </Col>
           </Row>
@@ -44,7 +37,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => bindActionCreators({
-  onGoogleSuccess: (response) => actions.login(response.tokenObj.id_token)
+  onGoogleSuccess: (response) => actions.login(response.credential)
 }, dispatch);
 
 const onGoogleFailure = (response) => console.log(response);
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import store from './singletons/store';
 import apolloClient from './singletons/apolloClient';
 import {unregister as unregisterServiceWorker} from './registerServiceWorker';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import {ApolloProvider} from 'react-apollo';
 
 const render = () => {
   const App = require('./App').default;
   ReactDOM.render((
-    <ApolloProvider store={store} client={apolloClient}>
-      <App />
-    </ApolloProvider>
+    <GoogleOAuthProvider  clientId='914978031481-bk8e8bj1ur0vhq4qlh7n7875drin9r0e.apps.googleusercontent.com'> 
+      <ApolloProvider store={store} client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    </GoogleOAuthProvider> 
   ), document.getElementById('root'));
 };
 
