c30773272eabdfb77427bec9ff26ea7f5522546b
Michal Komora <56072239+misosviso@users.noreply.github.com>
2023-03-10 08:22:01 +0100

    Fix login testing (#81)
    * Initialize google object in window
    
    * Initialize window object in login-page.component.spec.ts
    
    * remove providers from configuring testing module
    
    * remove unused import

--- angular/src/app/login-page/login-page.component.spec.ts
+++ angular/src/app/login-page/login-page.component.spec.ts
@@ -1,7 +1,6 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { mockStoreModule } from '../mocks/store.mock';
-
 import { LoginPageComponent } from './login-page.component';
 
 describe('LoginPageComponent', () => {
@@ -10,6 +9,15 @@ describe('LoginPageComponent', () => {
 
   beforeEach(
     async(() => {
+      // init window.google object
+      window['google'] = {
+        accounts: {
+          id: {
+            initialize: () => {},
+            renderButton: () => {},
+          },
+        },
+      };
       TestBed.configureTestingModule({
         declarations: [LoginPageComponent],
         imports: [ReactiveFormsModule, mockStoreModule()],
